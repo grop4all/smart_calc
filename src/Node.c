609d61f0d7cd02fd181c386afc9fd3fa990f33a8
henschel <henschel@student.21-school.ru>
2022-12-04 18:26:26 +0500

    backend in readi

---
+++ src/Node.c
@@ -0,0 +1,59 @@
+#include "./Node.h"
+
+void push(Node_t **head, T value) {
+  Node_t *tmp = malloc(sizeof(Node_t));
+
+  if (tmp == NULL) {
+    exit(STACK_OVERFLOW);
+  }
+  tmp->next = *head;
+  tmp->value = value;
+  *head = tmp;
+}
+
+Node_t *pop1(Node_t **head) {
+  Node_t *out;
+  if ((*head) == NULL) {
+    exit(STACK_UNDERFLOW);
+  }
+  out = *head;
+  *head = (*head)->next;
+  return out;
+}
+
+T pop2(Node_t **head) {
+  Node_t *out;
+  T value;
+  if (*head == NULL) {
+    return '\2';
+  }
+  out = *head;
+  *head = (*head)->next;
+  value = out->value;
+  free(out);
+  return value;
+}
+
+T peek(const Node_t *head) {
+  if (head == NULL) {
+    return '\2';
+  }
+  return head->value;
+}
+
+void printStack(const Node_t *head) {
+  printf("stack >");
+  while (head) {
+    printf("%d ", head->value);
+    head = head->next;
+  }
+}
+
+size_t getSize(const Node_t *head) {
+  size_t size = 0;
+  while (head) {
+    size++;
+    head = head->next;
+  }
+  return size;
+}
---
+++ src/calc.h
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+#define MaxLemmaLen 7
+
+typedef struct func {
+    char* func_name;
+    char short_name;
+} t_func;
+
+int pars(char *line, char *out);
+
+t_func set_func[] = {
+    {"cos", 'c'},
+    {"sin", 's'},
+    {"tan", 't'},
+    {"acos",'a'},
+    {"asin", 'b'},
+    {"atan", 'i'},
+    {"ln", 'd'},
+    // "cos",
+    // "sin",
+    // "tan",
+    // "acos",
+    // "asin",
+    // "atan",
+    // "sqrt",
+    // "ln",
+    // "log",
+    {NULL, 0}
+};
+
+enum set_error {
+    OK,
+    link_null,
+    OUT_OF_MEMORY,
+};
+
